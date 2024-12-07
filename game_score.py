1d3b8bf09b971c475f6a06918b78dff1302aceea
Devon <yhes3103@gmail.com>
2024-12-07 11:48:10 +0800

    add score 7seg

--- game_score.py
+++ game_score.py
@@ -1,5 +1,10 @@
 import time
+import tm1637
 
+CLK = 22
+DIO = 23
+display = tm1637.TM1637(clk=CLK, dio=DIO)
+display.brightness(7)
 file_path = "game_score.txt"
 
 counter = 1
@@ -8,6 +13,7 @@ try:
         with open(file_path, "w") as file:
             file.write(str(counter))
         print(f"Written: {counter}") 
+        display.show(str(counter).zfill(4))
         counter += 1
         time.sleep(1)  
 except KeyboardInterrupt:
